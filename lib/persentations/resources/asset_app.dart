95c71d6ff35f3ff79689ab56da7a929da4d8b65c
efrilm_ <macbook@Efril-Mac.local>
2024-07-06 08:38:31 +0700

    Added Assets App

---
+++ lib/persentations/resources/asset_app.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: constant_identifier_names
+
+const IMAGE_APP = "assets/images";
+const ICON_APP = "assets/icons";
+
+class ImageApp {
+  // Dummy Image
+  static const String dmEqualTee = "$IMAGE_APP/dm-equal-tee.png";
+  static const String dmReducing = "$IMAGE_APP/dm-reducing.png";
+}
+
+class IconApp {
+  static const String historyStock = "$ICON_APP/history-stock.svg";
+  static const String product = "$ICON_APP/product.svg";
+  static const String purchaseItem = "$ICON_APP/purchase-item.svg";
+  static const String requestItem = "$ICON_APP/request-item.svg";
+  static const String stockOpname = "$ICON_APP/stock-opname.svg";
+}
