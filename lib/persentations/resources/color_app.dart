58b28b0aaeb758f56ed5ab2c5091ef5d2834fa44
efrilm_ <macbook@Efril-Mac.local>
2024-07-06 08:27:33 +0700

    Added Structure Folder and Color App

---
+++ lib/persentations/resources/color_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const Color primary = Color(0xFF417DF3);
+  static const Color black = Color(0xFF000000);
+  static const Color blackText = Color(0xFF1E1E1E);
+  static const Color grey = Color(0xFFF5F5F5);
+  static const Color greyText = Color(0xFF757575);
+  static const Color border = Color(0xFFD9D9D9);
+  static const Color yellow = Color(0xfff8e153);
+  static const Color green = Color(0xff00863F);
+  static const Color greenBg = Color(0xffE1F5EA);
+  static const Color red = Color(0xffEC221F);
+  static const Color redBg = Color(0xffFEE9E7);
+  static const Color brown = Color(0xffBF6A02);
+  static const Color brownBg = Color(0xffFFF5E9);
+}
